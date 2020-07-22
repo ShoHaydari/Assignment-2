ed24fdbba943741e8ef1b6e3ce6b29c30e8c14a8
ShoHaydari <68448470+ShoHaydari@users.noreply.github.com>
2020-07-22 13:52:06 -0400

    Update Account.cpp

--- Account.cpp
+++ Account.cpp
@@ -1,29 +1,36 @@
 
 #include "Account.hpp"
 #include <iostream>
+#include <stdexcept>
 using namespace std;
 
-Account::Account(double d){
+Account::Account(double d){ //constructor takes a double and sets it to the balance if the double is bigger or equal than 0. Else an invalid argument is thrown.
+    
     if (d>=0.0) {
         setBalance(d);
     }
     
     else{
-        balance=0;
-        cout<<"Error, balance should be set bigger or equal than 0.0";
+        throw invalid_argument("Balance must be bigger or equal than 0.0");
     }
 }
 
-void Account::setBalance(double c){
+void Account::setBalance(double c){ //sets the balance to the double entered.
     
     balance=c;
     
 }
-void Account::credit(double y){
-    
-    balance=balance+y;
+void Account::credit(double y){ //adds a positive double to the balance
+    if (y>0.0) {
+        balance=balance+y;
+    }
+    else {
+        throw invalid_argument ("The amount for credit needs to be bigger than 0.0");
+    }
 }
-bool Account::debit(double x){ 
+
+bool Account::debit(double x) //boolean function that withdraw a double x, if the double is less or equal that the double x. 
+{
     if (x>balance) {
         cout<<"The amount wanted is bigger than the balance"<<endl;
         return false;
@@ -33,8 +40,7 @@ bool Account::debit(double x){
         return true;
     }
 }
-double Account:: getBalance(){
-    
+
+double Account:: getBalance(){ //prints the value of the balance
     return balance;
 }
-
