a9d582cf4206f943e9f70dfb995de11392a671b2
ShoHaydari <68448470+ShoHaydari@users.noreply.github.com>
2020-07-22 13:51:30 -0400

    Update Account.h

--- Account.h
+++ Account.h
@@ -6,15 +6,19 @@ class Account{
     
 public:
     
-    Account(double);
-    void credit(double);
-    bool debit(double);
-    void setBalance(double);
-    double getBalance();
+    Account(double); //default constructor
+    
+    void credit(double); //This function allows to add a certain amount to the balance
+    
+    bool debit(double); //This function allows to withdraw a certain amount form the balance
+    
+    void setBalance(double); //This function sets the balance of the account
+    
+    double getBalance(); //returns the balance
     
 private:
     
-    double balance;
+    double balance; 
     
 
 };
