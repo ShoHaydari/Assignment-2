f1ce894ee17695d37501ec4737f1ae84ed93fc8a
ShoHaydari <68448470+ShoHaydari@users.noreply.github.com>
2020-07-22 13:48:57 -0400

    Update SavingAccount.cpp

--- SavingAccount.cpp
+++ SavingAccount.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
+#include <stdexcept>
 #include "SavingsAccount.hpp"
 using namespace std;
 
-SavingAccounts::SavingAccounts(double x, double y): Account(x){
+SavingAccounts::SavingAccounts(double x, double y): Account(x)
+//Constructor takes two double: x and y. x is set to the balance by the Account constructor and y is set to the interest rate if bigger than 0.0.
+{
     if (y>0.0) {
         interestRate=y;
     }
     else{
-        cout<<"Interest rate cannot be negative";
+        throw invalid_argument("Interest rate cannot be negative");
     }
 }
 
-void SavingAccounts::addInterest(){
-    credit(calculateInterest());
+double SavingAccounts::calculateInterest()
+//calculates the interest on the total balance by multiplying the balance and the interest rate. Prints the amount calculated interest rate.
+{
+    return getBalance()*interestRate;
 }
 
-double SavingAccounts::calculateInterest(){
-    
-    return getBalance()*interestRate;
-
+void SavingAccounts::addInterest()
+//adds the calculated interest rate in calculateInterest() to the balance using the credit function.
+{
+    credit(calculateInterest());
 }
